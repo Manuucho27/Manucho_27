c7ba1bc47918899f2666483715c3fd8af1a647ce
TuNombreApellidos <manudatos1@gmail.com>
2026-01-23 09:24:41 +0100

    modified:   css/style.css 	deleted:    html/amor-caducado.html 	new file:   html/amor-caducado.php 	renamed:    html/contacto.html -> html/contacto.php 	renamed:    html/disfruto.html -> html/disfruto.php 	renamed:    html/fueron.html -> html/fueron.php 	new file:   html/plantillas/footer.php 	new file:   html/plantillas/nav.php 	renamed:    html/ponte-cascos.html -> html/ponte-cascos.php 	renamed:    html/prisionero.html -> html/prisionero.php 	renamed:    html/si-responde-mis-stories.html -> html/si-responde-mis-stories.php 	renamed:    html/sobre-mi.html -> html/sobre-mi.php 	renamed:    html/temas.html -> html/temas.php 	renamed:    html/tienda.html -> html/tienda.php 	renamed:    html/una-chica.html -> html/una-chica.php 	renamed:    html/una-vez-alli.html -> html/una-vez-alli.php 	renamed:    html/vimos-madriz.html -> html/vimos-madriz.php 	deleted:    index.html 	new file:   index.php

---
+++ html/amor-caducado.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Manucho 27 - Amor caducado</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <?php include 'plantillas/nav.php'; ?>
+
+    <div class="container mt-5">
+        <h1>Amor caducado</h1>
+        <p>Contenido de la canción Amor caducado.</p>
+    </div>
+
+    <script src="../js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Manucho 27 - Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <?php include 'html/plantillas/nav.php'; ?>
+    <div id="pag_principal">
+        <!-- <img id="fondo" src="fotos/P1210213.jpg" alt="fondo"> -->
+        <div id="texto">
+            <h1>MANUCHO 27</h1>
+            <div id="bprin">
+                <a class="spoti" href="https://open.spotify.com/intl-es/artist/27K3MUgWcpbPj4RSYNxcew?si=709fe9d280204d84" target="_blank">SPOTIFY</a>
+                <a class="sm" href="biografia.html">SOBRE MÍ</a>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
